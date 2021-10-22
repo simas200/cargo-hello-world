f5af768f94039d3bca72bad8290d601127bcccc7
simas200 <siminskas@gmail.com>
2021-10-22 22:10:32 +0300

    Max function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+fn max(x: i32, y: i32) -> i32{
+    if (x > y) { 
+        return x
+    } else {
+        return y
+    }
+}
 fn main() {
     let x = 1;
+    let y = 5;
     println!("Value of x = {}", x);
+    println!("Max value = {}", max(x, y));
     println!("Hello, world!");
 }
